c23f2ede1de9b02b915c945b1eafe340f7f3e834
CannyAss <34702990+CannyAss@users.noreply.github.com>
2018-03-01 20:04:07 +0800

    Got the loop code to compile

--- Program_1/Program_1.ino
+++ Program_1/Program_1.ino
@@ -1,33 +1,32 @@
 void setup() {
   // put your setup code here, to run once:
   /* Record initial value of z, when d is within 3cm for 5 seconds (Using a x=x+1 function)
-   *  Emit a sound to indicate ready status
-   */
+      Emit a sound to indicate ready status
+  */
+
 }
 
 void loop() {
-  #include <iostream> 
-using namespace std;
-int main() {
-   int x, y, z, d; 
-   /* x is the number of push-ups
-    *  y is the 'down' counter
-    *  z is the 'up' status
-    *  d is the distance sensor
+
+  using namespace std;
+    int x, y, z, d;
+    /* x is the number of push-ups
+        y is the 'down' state
+        z is the 'up' position distance
+        d is the distance sensor
     */
 
-  
-  do{
-    if (d <= 6 || y == 0){
-      y++;
-    }
-    if ( d>z || y == 1){
-      x++;
-      y--;
-    }
-  } while ( d<z || d>5) /* entering push-up status, d is smaller than initial position while further than 5cm) */
+
+    do {
+      if (d <= 6 || y == 0) {
+        y++;
+      }
+      if ( d > z || y == 1) {
+        x++;
+        y--;
+      }
+    } while ( d < z || d > 5); /* entering push-up status, d is smaller than initial position while further than 5cm) */
 
 
-return 0;
-
+      return 0;
 }
